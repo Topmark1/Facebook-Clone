b8f87a39a305e34964ce27f3b47f0a59af3d651a
Topmark1 <topsiology11@gmail.com>
2023-01-30 04:43:34 +0100

    connect to firebase success

--- components/InputBox.js
+++ components/InputBox.js
@@ -6,11 +6,40 @@ import {
 import {
     CameraIcon, VideoCameraIcon
 } from "@heroicons/react/24/solid";
+import {useRef} from 'react';
+import {db} from '../firebase';
+import {serverTimestamp,collection,addDoc} from 'firebase/firestore';
 
 function InputBox(){
 const {data:session} = useSession();
+const inputRef = useRef(null);
+
 const sendPost =(e) =>{
 e.preventDefault();
+if(!inputRef.current.value) return;
+
+/*db.collection('users').add({
+message: inputRef.current.value,
+name:session.user.name,
+email:session.user.email,
+image:session.user.image,
+timestamp:serverTimestamp()
+});*/
+
+try {
+  const docRef = addDoc(collection(db, "users"), {
+    message: inputRef.current.value,
+name:session.user.name,
+email:session.user.email,
+image:session.user.image,
+timestamp:serverTimestamp(),
+  });
+
+} catch (e) {
+  console.error("Error adding document: ", e);
+}
+
+inputRef.current.value="";
 } ;
 
     return (
@@ -25,8 +54,9 @@ layout='fixed'
 />
 <form className="flex flex-1">
 <input 
-className="rounded-full h-12 bg-gray flex-grow px-5 focus:outline-none"
-type="text" 
+className="rounded-full h-12 bg-gray-100 flex-grow px-5 focus:outline-none"
+type="text"
+ref={inputRef} 
 placeholder={"what's on your mind, "+ session.user.name+'?'}
 />
 <button hidden className="" type="submit" onClick={sendPost}>Submit</button>
---
+++ firebase.js
@@ -0,0 +1,27 @@
+
+import {getFirestore} from "firebase/firestore";
+import {getStorage} from "firebase/storage";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp,getApps,getApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCWXuD0qXPNijlKcnrn_kE3q5z3mlL236M",
+  authDomain: "fakebuok-9be3f.firebaseapp.com",
+  projectId: "fakebuok-9be3f",
+  storageBucket: "fakebuok-9be3f.appspot.com",
+  messagingSenderId: "160157159593",
+  appId: "1:160157159593:web:fa3d6d7af4ae8307fa19b8"
+};
+
+// Initialize Firebase
+const app =!getApps().length?initializeApp(firebaseConfig):getApp();
+
+const db = getFirestore(app);
+const storage = getStorage();
+
+export {db, storage}
